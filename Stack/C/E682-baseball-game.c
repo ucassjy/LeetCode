1b2f580f05b46d0145cf52a88b49bb201340e113
ucassjy <470323747@qq.com>
2018-07-19 11:44:56 +0800

    Debug: correct oprations for stack

--- Stack/C/E682-baseball-game.c
+++ Stack/C/E682-baseball-game.c
@@ -6,11 +6,12 @@ typedef struct{
 	int top;
 }Stack;
 void Pop(Stack *s, int *n){
-	*n = s->stack_array[s->top];
 	s->top--;
+	*n = s->stack_array[s->top];
 }
 void Push(Stack *s, int n){
-	s->stack_array[++s->top] = n;
+	s->stack_array[s->top] = n;
+	s->top++;
 }
 int calPoints(char** ops, int opsSize){
 	Stack s;
@@ -22,9 +23,10 @@ int calPoints(char** ops, int opsSize){
 			Pop(&s, &t1);
 			score -= t1;
 		}else if(ops[i][0] == 'D'){
-			t1 = s.stack_array[s.top] * 2;
-			Push(&s, t1);
-			score += t1;
+			Pop(&s, &t1);
+			t2 = t1 * 2;
+			Push(&s, t1);  Push(&s, t2);
+			score += t2;
 		}else if(ops[i][0] == '+'){
 			Pop(&s, &t1);  Pop(&s, &t2);
 			t3 = t1 + t2;
