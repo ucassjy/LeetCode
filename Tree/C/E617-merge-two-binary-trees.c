b2610f33b0572f667918863dfecd7df83aefefa7
ucassjy <470323747@qq.com>
2018-07-23 16:40:06 +0800

    Add E617 for C and Python3

---
+++ Tree/C/E617-merge-two-binary-trees.c
@@ -0,0 +1,20 @@
+// time:  O(n)
+// space: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* mergeTrees(struct TreeNode* t1, struct TreeNode* t2) {
+    if(!t1 && t2)  return t2;
+    if(t1 && !t2)  return t1;
+    if(!t1 && !t2)  return NULL;
+    t1->val += t2->val;
+    t1->left = mergeTrees(t1->left, t2->left);
+    t1->right = mergeTrees(t1->right, t2->right);
+    return t1;
+}
